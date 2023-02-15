1f4a4eb328aff9fc8fa1a2ab3c15c49fdc3d286e
yedin123 <aas9919@naver.com>
2023-02-15 11:02:31 +0900

    first

---
+++ src/main/java/com/todoAplication/todoAplication/TodoAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.todoAplication.todoAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoAplication/todoAplication/TodoAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoAplication.todoAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
